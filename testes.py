cd275d2aa9bb1f591b6ab69f37b8df1ec6a1a8b2
juliotadeu <julio.silva@aluno.faculdadeimpacta.com.br>
2019-04-05 16:53:08 -0300

    novas modificações

--- testes.py
+++ testes.py
@@ -3,20 +3,22 @@ import sys
 
 erroInicializar = False
 
-
 jogo = jogovelha.inicializar()
 
 if len(jogo) != 3:
 	erroInicializar = True
+
 else:
 	for linha in jogo:
 		if len(linha) != 3:
 			erroInicializar = True
 		else:
 			for elemento in linha:
-				if elemento != 'X':
+				if elemento != '.':
 					erroInicializar = True
+
 if erroInicializar:
 	sys.exit(1)
+
 else:
 	sys.exit(0)
