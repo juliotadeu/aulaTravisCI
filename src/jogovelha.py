111c685709c24ea6c492c561080c736247ead993
juliotadeu <julio.silva@aluno.faculdadeimpacta.com.br>
2019-04-05 22:23:52 -0300

    nova tentativa

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -3,7 +3,7 @@ def inicializar():
     for i in range(3):
         linha = []
         for j in range(3):
-            linha.append(".") # Tente outra vez
+            linha.append(".") # NOVAMENTE
         tab.append(linha)
     return tab
 
